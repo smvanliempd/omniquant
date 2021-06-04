dd1b8175f78d888d3116205adae2195060984818
smvanliempd <smvanliempd@gmail.com>
2021-06-04 10:36:45 +0200

    only use concentrations if calibrants are present

--- functions/biol_conc.R
+++ functions/biol_conc.R
@@ -4,6 +4,7 @@
 biol.conc    <- function( dat ) {
   
   # Get data
+  n.calibrants <- length(dat$calibrants)
   full.dat <- dat$data
   assay.data <- dat$assay_pars
   vol_hom <- assay.data[Metric == "Homogenization_volume", Value] # µL
@@ -14,17 +15,20 @@ biol.conc    <- function( dat ) {
   #  calculate loss due to sample preparation
   loss_fact <- vol_hom/vol_sv
   
-  # calculate amount from original sample
-  full.dat[ Sample.Class == "Sample", Sample_amount := (loss_fact  * vol_res * Conc_raw_adj)] # pmol
-  
-  # calculate amount per tissue weight depending on whether values are MFC adjusted or not.
-  mets.mfc <- dat$mfc$included
-  full.dat[ Sample.Class == "Sample", Amount_mg_tissue := ifelse(Analytes %in% mets.mfc, 
-                                                                  Sample_amount/median(Weight),
-                                                                  Sample_amount/Weight)]
-  
-  # calculate concentrations per amount of cells
-  full.dat[ Sample.Class == "Sample" , Conc_biol := (loss_fact  * vol_res * Conc_raw_adj)/cell_nr] #nmol/million cells
+  if (n.calibrants > 0) {
+    
+    # calculate amount from original sample
+    full.dat[ Sample.Class == "Sample", Sample_amount := (loss_fact  * vol_res * Conc_raw_adj)] # pmol
+    
+    # calculate amount per tissue weight depending on whether values are MFC adjusted or not.
+    mets.mfc <- dat$mfc$included
+    full.dat[ Sample.Class == "Sample", Amount_mg_tissue := ifelse(Analytes %in% mets.mfc, 
+                                                                   Sample_amount/median(Weight),
+                                                                   Sample_amount/Weight)]
+    
+    # calculate concentrations per amount of cells
+    full.dat[ Sample.Class == "Sample" , Conc_biol := (loss_fact  * vol_res * Conc_raw_adj)/cell_nr] #nmol/million cells
+  }
   
   # correct signals for different resuspension and speedvac volumes in each assay
   full.dat[ Sample.Class == "Sample" , Signal_assay_norm := (loss_fact  * vol_res * Signal_MFC_QC)/100 ]
